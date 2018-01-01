e9d1b80a8e761d9b45d0cd932f7bd54beb57ae43
Samuel <sjoseph1848@gmail.com>
2018-01-01 17:09:14 -0600

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contactus' => 'pages#contactus'
+  get 'originstory' => 'pages#originstory'
+  get 'projects' => 'pages#projects'
+  get 'skills' => 'pages#skills'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
